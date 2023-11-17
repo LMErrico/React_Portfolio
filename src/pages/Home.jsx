d0dc56fff9d4588ee65539e7d987790147be38ef
Lima <linam.errico@gmail.com>
2023-11-16 20:03:54 -0500

    added photo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,13 @@ export default function Home() {
   return (
     <div>
       <h1>About Me</h1>
+      <div>
+      <img
+        src="/images/photo.jpg" 
+        alt="photo" 
+        style={{ width: '200px', height: '200px', borderRadius: '50%' }}
+      />
+      </div>
       <p>
       I am a Colombian lawyer with a strong legal background. I have excelled in overseeing legal teams through effective information handling, managing high-profile cases, and providing crucial support during times of crisis. My dedication to excellence, coupled with a commitment to continuous learning, defines my professional ethos. 
 
